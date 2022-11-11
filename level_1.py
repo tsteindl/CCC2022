d103beb6d0a2bd78b4731c7636d40820a5d6a914
tsteindl <tobias.steindl@gmx.net>
2022-11-11 15:24:44 +0100

    read input file

--- level_1.py
+++ level_1.py
@@ -1,4 +1,19 @@
-n = input("n: ")
-matrix_input = input("matrix: ")
+with open('level1_1.in') as f:
+    lines = f.readlines()
 
+
+matrix = []
+for i, line in enumerate(lines):
+    if i == 0:
+        n = line
+        continue
+    matrix.append(list(line.rstrip()))
+
+
+
+#for i in range(n):
+#    matrix[i] = list(lines[i])
+
+
+print(matrix)
 
